2529259feb5a93e30d033a5acd6f320bb86208de
oliadler <oliver_elias.adler@fh-bielefeld.de>
2022-11-04 20:04:39 +0100

    Init Commit

--- src/Vergleich.java
+++ src/Vergleich.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class Vergleich {
+public class Vergleich {
+
+    private boolean sameSize;
+    private boolean samePairs;
+    private boolean equal;
+
+    public boolean isEqual(int[] a, int[] b) {
+
+        sort(a);
+        sort(b);
+
+        if (a.length == b.length) {
+            sameSize = true;
+
+        }
+
+
+        //{1,3,3,5};
+        //{1,3,3,5};
+
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                if (a[i] == b[i]) {
+                    samePairs = true;
+                    break;
+
+                } else {
+                    samePairs = false;
+                    break;
+                }
+            }
+
+        }
+
+        if (sameSize && samePairs) {
+            equal = true;
+        }
+        return equal;
+    }
+
+
+    public int[] sort(int[] unsorted) {
+
+        for (int i = 1; i < unsorted.length; i++) {
+
+            for (int j = 0; j < unsorted.length - i; j++) {
+                int temp;
+
+                if (unsorted[j] > unsorted[j + 1]) {
+                    temp = unsorted[j];
+                    unsorted[j] = unsorted[j + 1];
+                    unsorted[j + 1] = temp;
+                }
+            }
+        }
+
+        return unsorted;
+    }
 }
--- src/VergleichTest.java
+++ src/VergleichTest.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class VergleichTest {
+public class VergleichTest {
+
+    public static void main(String[] args) {
+
+        int [] a = {3,5,1,3};
+        int [] b = {1,5,3,3};
+        boolean isEqual;
+
+        Vergleich vergleich = new Vergleich();
+        isEqual = vergleich.isEqual(a,b);
+
+        System.out.println("Output: " + isEqual);
+
+
+
+
+    }
 }
